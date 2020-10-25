3303c632b703cf7b9122cfb0463f82c525cd3da1
Allen Nicholson <ranicholson@git.com>
2020-10-25 10:16:12 -0500

    Test File

--- main1.c
+++ main1.c
@@ -3,7 +3,17 @@
 int main(void)
 {
 	char t = 't';
+	char str[] = "Hello\n";
+	int a = 100;
+	int b = -100;
+	int c = 0x09;
+	int d = 010;
 
-	_printf("Hello %c", t);
+	_printf("Hello %c\n", t);
+	_printf("%s", str);
+	_printf("%%\n");
+	_printf("\\ \' \" \?\n");
+	_printf("%d, %d, %i, %i\n", a, b, c, d);
+
 	return (0);
 }
