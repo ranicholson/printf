216b1a0f16e77714fecc79c965a0343f268de188
mlaizure <maddi.laizure@gmail.com>
2020-10-26 23:44:34 +0000

    Added 0 address logic

--- format_p.c
+++ format_p.c
@@ -9,12 +9,15 @@ int format_p(void *p)
 	int cc = 0;
 	unsigned long int n;
 
+	n = (unsigned long int)p;
+
+	if (n == 0)
+		return (_printf("(nil)"));
+
 	_putchar('0');
 	cc++;
 	_putchar('x');
 	cc++;
-
-	n = (unsigned long int)p;
 
 	cc += format_p_helper(n);
 
