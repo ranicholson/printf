34f8c5a81b5cc96088eee7706c79b0177cde1589
mlaizure <maddi.laizure@gmail.com>
2020-10-27 21:13:11 +0000

    Moved format_p_helper funciton into format_p file

--- format_p.c
+++ format_p.c
@@ -28,3 +28,32 @@ int format_p(void *p)
 	return (cc);
 
 }
+
+/**
+ * format_p_helper - prints address of void pointer in hexadecimal
+ * @n: address of void pointer
+ *
+ * Return: number of characters printed
+ */
+
+int format_p_helper(unsigned long int n)
+{
+	int cc = 0, rem = 0;
+
+	if (n / 16)
+		cc += format_p_helper(n / 16);
+
+	rem = n % 16;
+
+	if (rem < 10)
+	{
+		_putchar(rem + '0');
+		cc++;
+	}
+	else
+	{
+		_putchar(rem - 10 + 'a');
+		cc++;
+	}
+	return (cc);
+}
