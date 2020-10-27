c0561915801373d81d75422c1b1d5a4ffede0aa8
mlaizure <maddi.laizure@gmail.com>
2020-10-27 21:18:42 +0000

    Moved print_number10 function into format_di file

--- format_di.c
+++ format_di.c
@@ -15,3 +15,37 @@ int format_di(int a)
 
 	return (cc);
 }
+
+/**
+ * print_number10 - prints an integer in base 10
+ * @n: integer to print
+ *
+ * Return: number of characters printed
+ */
+
+int print_number10(int n)
+{
+	int cc = 0;
+
+	if (n / 10 == 0 && n < 0)
+	{
+		_putchar('-');
+		cc++;
+	}
+
+	if (n / 10)
+		cc += print_number10(n / 10);
+
+	if (n % 10 < 0)
+	{
+		_putchar(-(n % 10) + '0');
+		cc++;
+	}
+
+	else
+	{
+		_putchar(n % 10 + '0');
+		cc++;
+	}
+	return (cc);
+}
